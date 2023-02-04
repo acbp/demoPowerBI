aa48f72235ac42af2a80343193f4428d17be4412
Alexandre Calil Blasizza Paravani <alexandrevil@gmail.com>
2023-02-04 13:51:59 -0300

    fix mssql connection

--- accessPoint/index.js
+++ accessPoint/index.js
@@ -1,78 +1,79 @@
-const DB_URL="172.17.0.2"
-const DB_USER="sa";
+const DB_URL="localhost";
+const DB_USER="SA";
 const DB_PASS="@1234567890Developer"
 
-var node_mssql = require('node-mssql');
-var queryObj = new node_mssql.Query({
-    host: '0.0.0.0',	 // You can use 'x.x.x.x\\instance' to connect to named instance
-    port: 1433,
-    username: 'sa',
-    password: '@1234567890Develop',
-    database: 'master'
-});
-
-queryObj.table('dbo.mytable');
-return;
 var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
 
 var config = {
-  server: `${DB_URL}`,
-  options: {},
-  authentication: {
-    type: "default",
-    options: {  
-      userName: `${DB_USER}`,
-      password: `${DB_PASS}`,
+    server: `${DB_URL}`,
+    options: {
+        database: 'AdventureWorks2017',
+        trustServerCertificate: true
+    },
+    authentication: {
+        type: "default",
+        options: {  
+            userName: `${DB_USER}`,
+            password: `${DB_PASS}`,
+        }
     }
-  }
 };
 
 var connection = new Connection(config);
 
 // Setup event handler when the connection is established. 
-connection.on('connect', function(err) {
-  if(err) {
-    console.log('Error: ', err)
-    return;
-  }
-  // If no error, then good to go...
-  executeStatement();
-  // criar http server
+connection.on('connect', async function(err) {
+    if(err) {
+        console.log('Error: ', err)
+        return;
+    }
+
+    try{
+        console.log('connected !')
+        executeStatement(
+        `select table_name from AdventureWorks2017.information_schema.tables`
+        );
+    }
+    catch(e){
+        console.log(e)
+    }
+    // criar http server
 });
 
 // Initialize the connection.
 connection.connect();
 
-function executeStatement() {
-  const request = new Request('select * from MyTable', (err, rowCount) => {
-    if (err) {
-      throw err;
-    }
+function executeStatement(sql) {
+    const request = new Request(`${sql}`, (err, rowCount) => {
+        if (err) {
+            throw err;
+        }
 
-    console.log('DONE!');
-    connection.close();
-  });
-
-  // Emits a 'DoneInProc' event when completed.
-  request.on('row', (columns) => {
-    columns.forEach((column) => {
-      if (column.value === null) {
-        console.log('NULL');
-      } else {
-        console.log(column.value);
-      }
+        console.log('DONE!');
+        connection.close();
     });
-  });
 
-  request.on('done', (rowCount) => {
-    console.log('Done is called!');
-  });
+    // Emits a 'DoneInProc' event when completed.
+    request.on('row', (columns) => {
+        columns.forEach((column) => {
+            if (column.value === null) {
+                console.log('NULL');
+            } else {
+                console.log(column.value);
+            }
+        });
+    });
 
-  request.on('doneInProc', (rowCount, more) => {
-    console.log(rowCount + ' rows returned');
-  });
+    request.on('done', (rowCount) => {
+        console.log('Done is called!');
+    });
 
-  // In SQL Server 2000 you may need: connection.execSqlBatch(request);
-  connection.execSql(request);
+    request.on('doneInProc', (rowCount, more) => {
+        console.log(rowCount + ' rows returned');
+    });
+
+    // In SQL Server 2000 you may need: connection.execSqlBatch(request);
+    connection.execSql(request);
 }
 
